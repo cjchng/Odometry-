d792ed570cb2f542b6219eb8e83f504984db8a5a
Shivani <44637756+Shivani1796@users.noreply.github.com>
2020-02-13 23:13:14 +0800

    Add files via upload

---
+++ mez/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lopencv_ml -lopencv_dnn -lopencv_stitching -lopencv_video -lopencv_videoio -lopencv_videostab -lopencv_photo -lopencv_highgui -lopencv_calib3d -lopencv_imgcodecs -lopencv_imgproc -lopencv_core -lopencv_objdetect -lopencv_superres -lopencv_shape -lopencv_features2d -lopencv_flann
+
